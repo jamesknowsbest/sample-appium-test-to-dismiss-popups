6425056954761dc0f1d755bda39973896248ab2d
jpalmisano <jamesmpalmisano@gmail.com>
2017-10-22 20:00:55 -0400

    modified code to show how to click allow and deny in the permission popup

--- src/main/java/com/jamesknowsbest/www/SampleTest.java
+++ src/main/java/com/jamesknowsbest/www/SampleTest.java
@@ -3,11 +3,12 @@ package com.jamesknowsbest.www;
 
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import java.net.MalformedURLException;
-import java.net.URL;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class SampleTest {
@@ -29,10 +30,30 @@ public class SampleTest {
 
   @Test
   public void sampleTest() {
+	  
     MobileElement el1 = (MobileElement) driver.findElementByAccessibilityId("ReferenceApp");
     el1.click();
     MobileElement el2 = (MobileElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.support.v4.widget.DrawerLayout/android.widget.RelativeLayout/android.support.v7.widget.RecyclerView/android.widget.FrameLayout[8]");
     el2.click();
+    MobileElement el3 = (MobileElement) driver.findElementById("com.android.packageinstaller:id/permission_allow_button");
+    if (el3.isDisplayed()){
+    	el3.click();
+    }else{
+    	System.out.println("Permission is not displayed");
+    }
+    
+    
+    
+//    MobileElement el1 = (MobileElement) driver.findElementByAccessibilityId("ReferenceApp");
+//    el1.click();
+//    MobileElement el2 = (MobileElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.support.v4.widget.DrawerLayout/android.widget.RelativeLayout/android.support.v7.widget.RecyclerView/android.widget.FrameLayout[8]");
+//    el2.click();
+//    MobileElement el3 = (MobileElement) driver.findElementById("com.android.packageinstaller:id/permission_deny_button");
+//    el3.click();
+
+    
+    
+    
   }
 
   @After
